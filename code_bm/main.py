d26d4bf47d622adcc95f71df7bb0bd1445c7bfdb
Khaled <pip139@protonmail.com>
2020-02-20 22:10:02 +0100

    bos

--- code_bm/main.py
+++ code_bm/main.py
@@ -1,19 +1,44 @@
-import numpy
+#import numpy
+import Book
+import Library
 
 # reading input file
-filename = 'a_example.txt'
-f = open(filename, "r")
+input_filename = 'a_example.txt'
+input_file = open(input_filename, "r")
+total_books, total_libraries, total_days = input_file.readline().split()
+total_books, total_libraries, total_days = int(total_books) , int(total_libraries), int(total_days)
 
-line = f.readline()
+book_scores = input_file.readline().split()
+index = 0
+all_books = []
+for book_score in book_scores:
+    book = Book.Book(index, int(book_score))
+    all_books.append(book)
+    index += 1
 
-#
-# read in input files
-#
 
-B, L, D = int(line[0]), int(line[1]), int(line[2])
+lib_index = 0
+line2 = line1 = "some line"
+all_libraries = []
+while True:
+    line1 = input_file.readline()
+    line2 = input_file.readline() if line1 else None
+
+    if not line1 or not line2:
+        break
+    lib_books_indices = [int(i) for i in line2.split()]
+    lib_books = [all_books[i] for i in lib_books_indices]
+    number_of_books, signup_days, books_per_day = line1.split()
+    number_of_books, signup_days, books_per_day = int(number_of_books), int(signup_days), int(books_per_day)
+
+    lib = Library.Library(lib_index, signup_days, books_per_day, number_of_books, lib_books)
+    all_libraries.append(lib)
+
+    lib_index += 1
 
 
-
+print(all_libraries)
+print(all_libraries[0].books)
 # Class Book
 # Book parameters
 # ID, score
@@ -40,4 +65,3 @@ B, L, D = int(line[0]), int(line[1]), int(line[2])
 
 # Similarity Matrix (cheap)
 #   => penalty for the LSS
-
